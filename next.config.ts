f1c878c826a1c8a5eb230662479626587b80488d
Jflyfish <126128074+Jflyfish@users.noreply.github.com>
2026-05-20 18:12:17 -0400

    Add Impact-Site-Verification response header

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Impact-Site-Verification',
+            value: 'e7de6a6f-016b-4648-9f9c-158540f33c60',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
